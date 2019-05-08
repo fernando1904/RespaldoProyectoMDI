1c5a56559e6f74d3465ce5c58e6aa1d396498c16
Fernando <48523897+fernando1904@users.noreply.github.com>
2019-05-08 20:24:32 -0300

    Version 2 - Mauricio Fontana

--- Encriptador/src/Encriptador/Tablero.java
+++ Encriptador/src/Encriptador/Tablero.java
@@ -1,5 +1,6 @@
 package Encriptador;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -11,6 +12,11 @@ import javax.swing.JPanel;
 
 public class Tablero extends JPanel implements ActionListener{
     
+    private int contador;
+    private int contadorCod;
+    public static int cod[][] = new int[4][8]; 
+    private int con1 = 0;
+    private int con2 = 0;
     
     private JButton a1 = new JButton("1");
     private JButton a2 = new JButton("2");
@@ -88,6 +94,202 @@ public class Tablero extends JPanel implements ActionListener{
         i.addActionListener(this);
     }
     
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("a1")){
+        cod[con1][con2] = 1;
+        procesoBoton(a1);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a2")){
+        cod[con1][con2] = 2;
+        procesoBoton(a2);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a3")){
+        cod[con1][con2] = 3;
+        procesoBoton(a3);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a4")){
+        cod[con1][con2] = 4;
+        procesoBoton(a4);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a5")){
+        cod[con1][con2] = 5;
+        procesoBoton(a5);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a6")){
+        cod[con1][con2] = 6;
+        procesoBoton(a6);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a7")){
+        cod[con1][con2] = 7;
+        procesoBoton(a7);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("a8")){
+        cod[con1][con2] = 8;
+        procesoBoton(a8);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b1")){
+        cod[con1][con2] = 9;
+        procesoBoton(b1);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b2")){
+        cod[con1][con2] = 10;
+        procesoBoton(b2);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b3")){
+        cod[con1][con2] = 11;
+        procesoBoton(b3);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b4")){
+        cod[con1][con2] = 12;
+        procesoBoton(b4);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b5")){
+        cod[con1][con2] = 13;
+        procesoBoton(b5);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b6")){
+        cod[con1][con2] = 14;
+        procesoBoton(b6);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b7")){
+        cod[con1][con2] = 15;
+        procesoBoton(b7);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("b8")){
+        cod[con1][con2] = 16;
+        procesoBoton(b8);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c1")){
+        cod[con1][con2] = 17;
+        procesoBoton(c1);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c2")){
+        cod[con1][con2] = 18;
+        procesoBoton(c2);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c3")){
+        cod[con1][con2] = 19;
+        procesoBoton(c3);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c4")){
+        cod[con1][con2] = 20;
+        procesoBoton(c4);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c5")){
+        cod[con1][con2] = 21;
+        procesoBoton(c5);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c6")){
+        cod[con1][con2] = 22;
+        procesoBoton(c6);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c7")){
+        cod[con1][con2] = 23;
+        procesoBoton(c7);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("c8")){
+        cod[con1][con2] = 24;
+        procesoBoton(c8);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d1")){
+            cod[con1][con2] = 25;
+            procesoBoton(d1);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d2")){
+        cod[con1][con2] = 26;
+        procesoBoton(d2);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d3")){
+        cod[con1][con2] = 27;
+        procesoBoton(d3);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d4")){
+        cod[con1][con2] = 28;
+        procesoBoton(d4);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d5")){
+        cod[con1][con2] = 29;
+        procesoBoton(d5);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d6")){
+        cod[con1][con2] = 30;
+        procesoBoton(d6);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d7")){
+        cod[con1][con2] = 31;
+        procesoBoton(d7);
+        Contadores();
+        }
+        if (e.getActionCommand().equals("d8")){
+        cod[con1][con2] = 32;
+        procesoBoton(d8);
+        Contadores();
+        }
+    }
+    
+    final void Contadores() {
+        contadorCod++;
+        if (contadorCod % 8 == 0) {
+            // contadorCod = 0
+            contador++;
+        }
+        con2++;
+        
+        if ((con2 == 8)) {
+            con2 = 0;
+            con1++;
+        }
+    }
+    
+    final void procesoBoton(JButton i) {
+        switch (contador) {
+            case 0:
+                i.setBackground(Color.green);
+                break;
+            case 1:
+                i.setBackground(Color.red);
+                break;
+            case 2:
+                i.setBackground(Color.yellow);
+                break;
+            case 3:
+                i.setBackground(Color.blue);
+                break;
+        }
+        i.setText(String.valueOf(contadorCod + 1));
+        i.setEnabled(false);
+    }
+    
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Encriptador");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -109,7 +311,5 @@ public class Tablero extends JPanel implements ActionListener{
         createAndShowGUI();
     }
 
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    
 }
