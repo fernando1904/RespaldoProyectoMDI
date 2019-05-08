6d78a3de26d10331182510377a7b6a747e1fd80e
Fernando <48523897+fernando1904@users.noreply.github.com>
2019-05-08 20:26:31 -0300

    Version 5 - Fernando Rocha

--- Encriptador/src/Encriptador/Encriptador.java
+++ Encriptador/src/Encriptador/Encriptador.java
@@ -16,16 +16,22 @@ public class Encriptador extends JPanel implements ActionListener {
     
     private String encresult="";
     private int cod[][] = new int[4][8];
-    private String texto="";
-    private int tamTexto;
-    private char char1;
-    private char char2;
-    private char char3;
-    private char char4;
+    private int bin1[][] = new int[4][8];
+    private int binCod[][] = new int[4][8];
     private int v1=0;
     private int v2=0;
     private int v3=0;
     private int v4=0;
+    private int index1=0;
+    private int index2=0;
+    private String texto="";
+    private int tamTexto;
+    private int dato=0;
+    private int con1 = 7;
+    private char char1;
+    private char char2;
+    private char char3;
+    private char char4;
     private Tablero tablero1 = new Tablero();
     
     protected JTextArea Visor1;
@@ -62,7 +68,7 @@ public class Encriptador extends JPanel implements ActionListener {
             }
             this.extraerCaracteres();
             this.convertiraBinario(0);
-            //this.encriptacion(0);
+            this.encriptacion(0);
         }
 
         texto = encresult;
@@ -116,15 +122,100 @@ public class Encriptador extends JPanel implements ActionListener {
     }
 
     final void DecABn(int dec, int binNro, int tipo) {
-        System.out.println("dec "+dec);
-        System.out.println("binNro "+binNro);
-        System.out.println("tipo "+tipo);
+        int res;
+        con1 = 0;
+        while ((con1 < 8)) {
+            bin1[binNro][con1] = 0;
+            con1++;
+        }
+        con1 = 7;
+        if (tipo == 0) {
+           do{
+                res = dec % 2;
+                bin1[binNro][con1] = res;
+                dec = (int)dec / 2;
+                con1--;
+            }while(dec != 0);
+        }
+        con1 = 7;
+    }
+    
+    final void encriptacion(int tipo) {
+//        showMessageDialog(null, bin1[0][0] + " " + bin1[0][1] + " " + bin1[0][2] + " " + bin1[0][3] + " " + bin1[0][4] + " " + bin1[0][5] + " " + bin1[0][6] + " " + bin1[0][7]);
+        switch (tipo) {
+            case 0:
+                for (int i = 0; i <= 3; i++) {
+                    for (int j = 0; j <= 7; j++) {
+                        dato = cod[i][j];
+                        switch (dato) {
+                            case 1:
+                            case 2:
+                            case 3:
+                            case 4:
+                            case 5:
+                            case 6:
+                            case 7:
+                            case 8:
+                                index1 = 0;
+                                index2 = (dato - 1);
+                                break;
+                            case 9:
+                            case 10:
+                            case 11:
+                            case 12:
+                            case 13:
+                            case 14:
+                            case 15:
+                            case 16:
+                                dato -= 8;
+                                index1 = 1;
+                                index2 = (dato - 1);
+                                break;
+                            case 17:
+                            case 18:
+                            case 19:
+                            case 20:
+                            case 21:
+                            case 22:
+                            case 23:
+                            case 24:
+                                dato -= 16;
+                                index1 = 2;
+                                index2 = (dato - 1);
+                                break;
+                            case 25:
+                            case 26:
+                            case 27:
+                            case 28:
+                            case 29:
+                            case 30:
+                            case 31:
+                            case 32:
+                                dato -= 24;
+                                index1 = 3;
+                                index2 = (dato - 1);
+                                break;
+                        }
+                        binCod[i][j] = bin1[index1][index2];
+                    }
+                }
+                break;
+        }
+        for (int hg = 0; hg < binCod.length; hg++) {
+            for (int gh = 0; gh < binCod[0].length; gh++) {
+                System.out.print(binCod[hg][gh]);
+                if (gh==binCod[0].length-1) {
+                    System.out.print("\n");
+                }else{
+                    System.out.print("\t");
+                }
+                
+            }
+        }
     }
 
     final void crearBoton(JButton i, String j) {
-
             i.setPreferredSize(new Dimension(150, 30));
-            
 //            add(i, BorderLayout.CENTER);
             add(i);
 //            i.addActionListener(this);
